7ed555e91776e090ce7b7f36d8efbf37576a1340
beppemarazzi <beppemarazzi@gmail.com>
2020-01-07 18:41:58 +0100

    UnitTest for System.Text.Json support

---
+++ src/Uno.CodeGen.Tests/Given_ImmutableEntity.SystemTextJson.cs
@@ -0,0 +1,89 @@
+// ******************************************************************
+// Copyright � 2015-2018 nventive inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ******************************************************************
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+
+namespace Uno.CodeGen.Tests
+{
+	public partial class Given_ImmutableEntity
+	{
+		[TestMethod]
+		public void Immutable_When_Serializing_A_Using_SystemTextJson()
+		{
+			var json = JsonSerializer.Serialize(A.Default.WithEntity(x => null).ToImmutable());
+
+			json.Should().BeEquivalentTo("{\"T\":null,\"Entity\":null,\"IsSomething\":true,\"Metadata\":null}");
+		}
+
+		[TestMethod]
+		public void Immutable_When_Serializing_ABuilder_Using_SystemTextJson()
+		{
+			var json = JsonSerializer.Serialize(A.Default.WithEntity(x => null));
+
+			json.Should().BeEquivalentTo("{\"T\":null,\"Entity\":null,\"IsSomething\":true,\"Metadata\":null}");
+		}
+
+		[TestMethod]
+		public void Immutable_When_Deserializing_ABuilder_Using_SystemTextJson()
+		{
+			const string json = "{\"IsSomething\":false, \"T\":null, \"Entity\":{\"MyField1\":1, \"MyField2\":2}}";
+			var a = JsonSerializer.Deserialize<A.Builder>(json).ToImmutable();
+			a.Should().NotBeNull();
+			a.IsSomething.Should().BeFalse();
+			a.T.Should().BeNull();
+			a.Entity.Should().NotBeNull();
+			a.Entity­.MyField1.Should().Be(1);
+			a.Entity­.MyField2.Should().Be(2);
+		}
+
+		[TestMethod]
+		public void Immutable_When_Deserializing_A_Using_SystemTextJson()
+		{
+			const string json = "{\"IsSomething\":false, \"T\":null, \"Entity\":{\"MyField1\":1, \"MyField2\":2}}";
+			var a = JsonSerializer.Deserialize<A>(json);
+			a.Should().NotBeNull();
+			a.IsSomething.Should().BeFalse();
+			a.T.Should().BeNull();
+			a.Entity.Should().NotBeNull();
+			a.Entity­.MyField1.Should().Be(1);
+			a.Entity­.MyField2.Should().Be(2);
+		}
+
+		[TestMethod]
+		public void Immutable_When_Deserializing_ABuilder_WithNull_Using_SystemTextJson()
+		{
+			const string json = "{\"IsSomething\":false, \"T\":null, \"Entity\":null}";
+			var a = JsonSerializer.Deserialize<A.Builder>(json).ToImmutable();
+			a.Should().NotBeNull();
+			a.IsSomething.Should().BeFalse();
+			a.T.Should().BeNull();
+			a.Entity.Should().BeNull();
+		}
+
+		[TestMethod]
+		public void Immutable_When_Deserializing_A_WithNull_Using_SystemTextJson()
+		{
+			const string json = "{\"IsSomething\":false, \"T\":null, \"Entity\":null}";
+			var a = JsonSerializer.Deserialize<A>(json);
+			a.Should().NotBeNull();
+			a.IsSomething.Should().BeFalse();
+			a.T.Should().BeNull();
+			a.Entity.Should().BeNull();
+		}
+	}
+}
